4fabaef1ffbea7612be71fa100cd0ff80008fcca
pugetive <toddgehman@gmail.com>
2020-06-10 11:55:57 -0400

    Updates

---
+++ precache-manifest.0accb47f25ac1c91e118d9ed2417527e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948c0ac63bceeede3ca045e795757612",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58c0da7e81a3c89c59bc",
+    "url": "/monsters-rolodex/static/css/main.95e1dd85.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c0da7e81a3c89c59bc",
+    "url": "/monsters-rolodex/static/js/main.409ff3d5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
